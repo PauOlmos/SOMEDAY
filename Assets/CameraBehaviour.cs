b90f14e5009ef7c1c8fa6bddf40fda23de19e285
Pau <pau.olmos2002@gmail.com>
2024-02-04 01:17:44 +0100

    lock on boss dot + lockonposition camera
    Also simplified and optimized code

--- Assets/CameraBehaviour.cs
+++ Assets/CameraBehaviour.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 using UnityEngine;
 using Cinemachine;
 
 public class CameraBehaviour : MonoBehaviour
 {
     public Transform orientation;
-    public Transform player;
-    public Transform playerObj;
+    public Transform objective;
+    public Transform objectiveObj;
     public Rigidbody rb;
 
     public Transform boss;
@@ -18,20 +19,30 @@ public class CameraBehaviour : MonoBehaviour
     public float righthorizontal;
     public float rightvertical;
 
-    public bool lockOnBoss = false;
+    public Transform lockPosition;
+    public enum cameraState
+    {
+        onObjective,onBoss,onPosition,
+    }
 
+    public cameraState camState;
+    public Image bosslockDot;
+    public Camera mainCamera;
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-    }
-    
+        camState = cameraState.onObjective;
+        bosslockDot.gameObject.SetActive(false);
 
-    // Update is called once per frame
-    void Update()
+}
+
+
+// Update is called once per frame
+void Update()
     {
-        Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
+        Vector3 viewDir = objective.position - new Vector3(transform.position.x, objective.position.y, transform.position.z);
         orientation.forward = viewDir.normalized;
 
         float horizontalInput = Input.GetAxis("LeftHorizontal");
@@ -42,40 +53,71 @@ public class CameraBehaviour : MonoBehaviour
         Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
         if(inputDir != Vector3.zero)
         {
-            playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
+            objectiveObj.forward = Vector3.Slerp(objectiveObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
         }
 
         if (Input.GetButtonDown("LockBoss"))
         {
-            lockOnBoss = !lockOnBoss;
+            //CameraOnLock(lockPosition, boss);
+            if(camState == cameraState.onBoss)
+            {
+                camState = cameraState.onObjective;
+            }
+            else if(camState == cameraState.onObjective)
+            {
+                camState = cameraState.onBoss;
+            }
+
         }
 
-        if (lockOnBoss)
+        bosslockDot.gameObject.SetActive(camState == cameraState.onBoss);
+
+        switch (camState)
         {
-            Debug.Log(Angulo(player, boss));
-            gameObject.GetComponent<CinemachineFreeLook>().m_XAxis.m_MaxValue = Angulo(player, boss) + 45;
-            gameObject.GetComponent<CinemachineFreeLook>().m_XAxis.m_MinValue = Angulo(player, boss) - 45;
-            gameObject.GetComponent<CinemachineFreeLook>().m_XAxis.m_Wrap = false;
-        }
-        else
-        {
-            gameObject.GetComponent<CinemachineFreeLook>().m_XAxis.m_MaxValue = 180;
-            gameObject.GetComponent<CinemachineFreeLook>().m_XAxis.m_MinValue = -180;
-            gameObject.GetComponent<CinemachineFreeLook>().m_XAxis.m_Wrap = true;
+            case cameraState.onObjective:
+                CameraLimits(180,0, true);
+                break;
+            case cameraState.onBoss:
+                CameraLimits(45,Angulo(objective,boss), false);
+                bosslockDot.transform.position = mainCamera.GetComponent<Camera>().WorldToScreenPoint(boss.transform.position);
+            break;
+            case cameraState.onPosition:
+
+                CameraLimits(0,0, false);
+
+                gameObject.GetComponent<CinemachineFreeLook>().Follow = lockPosition;
+                gameObject.GetComponent<CinemachineFreeLook>().LookAt = objective.transform;
+
+            break;
+            
         }
 
     }
 
-    float Angulo(Transform player, Transform Boss)
+    float Angulo(Transform objective, Transform Boss)
     {
 
-        Vector3 direccion = player.forward;
+        Vector3 direccion = objective.forward;
 
         // Vector que va desde el objeto que mira hacia el otro objeto
-        Vector3 vectorAOtroObjeto = Boss.position - player.position;
+        Vector3 vecDic = Boss.position - objective.position;
 
         // Calcula el ángulo entre los dos vectores
-        return Vector3.Angle(direccion, vectorAOtroObjeto);
+        return Vector3.Angle(direccion, vecDic);
 
+    }
+
+    public void CameraOnLock(Transform position, Transform lookAt)
+    {
+        if (lookAt != null) objective = lookAt;
+        if (position != null) lockPosition = position;
+        camState = cameraState.onPosition;
+    }
+    
+    private void CameraLimits(int maxmin, float direction, bool wrap)
+    {
+        gameObject.GetComponent<CinemachineFreeLook>().m_XAxis.m_MaxValue = direction + maxmin;
+        gameObject.GetComponent<CinemachineFreeLook>().m_XAxis.m_MinValue = direction - maxmin;
+        gameObject.GetComponent<CinemachineFreeLook>().m_XAxis.m_Wrap = wrap;
     }
 }
