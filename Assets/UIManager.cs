e68bd03a0bbfd8c51ca5963603802cc843958ab3
Pau <pau.olmos2002@gmail.com>
2024-02-14 18:57:09 +0100

    UI SetUp

---
+++ Assets/UIManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public PlayerHp pHp;
+    public PassiveAbility ability;
+    public GameObject[] hearts;
+    public GameObject chargedPassive;
+    public GameObject actualPassive;
+    public Sprite activePassive;
+    public Sprite distanceAttack;
+    public Sprite restoreHp;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
