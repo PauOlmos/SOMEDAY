ec2bf43ce0e270e4990e4b2a6b9dfc9e07c38557
Pau Olmos Serrano <79161085+PauOlmos@users.noreply.github.com>
2024-06-04 23:43:10 +0200

    Credits set up
    Fade to alpha, credits, models, camera movment

--- Assets/Scripts/Camera/HospitalCamera.cs
+++ Assets/Scripts/Camera/HospitalCamera.cs
@@ -2,10 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HospitalCamera : MonoBehaviour
 {
     // Start is called before the first frame update
+    public AudioSource music;
+
+    public float musicTimer = 0.0f;
+    public float fadeBlackTimer = 0.0f;
 
     public Transform LookAtHeadPlayer;
 
@@ -35,6 +40,17 @@ public class HospitalCamera : MonoBehaviour
             gameObject.transform.position += direction.normalized * Time.deltaTime;
 
             if (Vector3.Distance(camPath[currentCamPos + 1].localPosition, gameObject.transform.localPosition) < 0.55f) currentCamPos++;
+        }
+
+        musicTimer += Time.deltaTime;
+
+        if (musicTimer >= music.clip.length)
+        {
+            fadeBlackTimer += Time.deltaTime / 3;
+            //music.Stop();
+            white.color = new Color(0, 0, 0, fadeBlackTimer);
+            if (fadeBlackTimer > 1.0f) SceneManager.LoadScene(5);
+
         }
     }
 }
